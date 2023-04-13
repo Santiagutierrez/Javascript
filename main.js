6c687090606ea3a3da942dc818395f16e273dbb2
Santiago Gutierrez <santiagogutierrezcode@gmail.com>
2023-04-13 20:46:33 -0300

    proyecto

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-let numeroUno = parseFloat(prompt("increse el primer numero"));
-let numeroDos = parseFloat(prompt("ingrese el segundo numero"));
+// let numeroUno = parseFloat(prompt("increse el primer numero"));
+// let numeroDos = parseFloat(prompt("ingrese el segundo numero"));
 
 
-alert (numeroUno + numeroDos)
+// alert (numeroUno + numeroDos)
+/*
+= asignacion
+== comparacion solo del valor
+=== comparacion del valor y el tipo
+*/
+
+let nombre = prompt ("ingrese su nombre")
+
+if (nombre != "") {
+    alert (`Hola ${nombre}`)
+}else {
+    alert ("Vuelva a ingresar su nombre")
+}
