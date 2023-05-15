75651dda022002524d8888e394f747ce27d45bec
Santiago Gutierrez <santiagogutierrezcode@gmail.com>
2023-05-15 12:07:40 -0300

    cambios

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,51 @@
-let edad = parseInt(prompt("Ingrese su edad"));
+// let edad = parseInt(prompt("Ingrese su edad"));
 
- if(edad < 14){
-   alert("No puedes entrar a este sitio web");
- }else if(edad < 18){
-   alert("Puedes entrar con la supervision de un adulto");
- }else{
-   alert("Puedes entrar a la pagina web");
- }
+//  if(edad < 14){
+//    alert("No puedes entrar a este sitio web");
+//  }else if(edad < 18){
+//    alert("Puedes entrar con la supervision de un adulto");
+//  }else{
+//    alert("Puedes entrar a la pagina web");
+//  }
 
- alert ("bienvenido al mundo de la fotografia analogica!")
-
-
- let cP = parseInt(prompt("Indique cuantas personas veran el sitio"));
- let eP;
- let sP = 0;
- let n;
-
- for (n=1;n<=cP;n++){
-  eP = parseInt(prompt("Ingrese la edad de la persona"+n));
-
-  sP = sP + eP;
- }
-
-  console.log (`El promedio de las edades de las ${cP} personas es: ${sP/cP}`);
+//  alert ("bienvenido al mundo de la fotografia analogica!")
 
 
-const suma = (a, b) => a + b;
-const resta = (a, b) => a - b;
-const iva = x => x * 0.21;
+//  let cantidadPersonas = parseInt(prompt("Indique cuantas personas veran el sitio"));
+//  let eP;
+//  let sP = 0;
+//  let n;
 
-let precioproducto = 1000;
-let descuento = 10;
+//  for (n=1;n<=cantidadPersonas;n++){
+//   eP = parseInt(prompt("Ingrese la edad de la persona"+n));
 
-let nuevoPrecio = resta(suma(precioproducto, iva(precioproducto)), descuento);
+//   sP = sP + eP;
+//  }
 
-console.log(precioproducto); 
+//   console.log (`El promedio de las edades de las ${cP} personas es: ${sP/cantidadPersonas}`);
+
+let camara = {
+    Marca: "Nikon",
+    Modelo: F2,
+    Año: 1985,
+    Disparos: 4536,
+    Garantia: false,
+};
+console.log(camara);
+
+let camara1 = {
+    Marca: "Canon",
+    Modelo: AE1,
+    Año: 1964,
+    Disparos: 10435,
+    Garantia: false,
+};
+console.log(camara1);
+
+let camara2 = {
+    Marca: "Zenit",
+    Modelo: zoomlight,
+    Año: 1954,
+    Disparos: 20365,
+};
+console.log(camara2);
